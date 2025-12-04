1bcf641bd93b3404d47e9cbe0457e250dffdc7ab
NaToKy <panser12@hotmail.com>
2025-12-04 12:12:33 -0500

    day 1

---
+++ day1.py
@@ -0,0 +1,15 @@
+f = open('input_day_1.txt', 'r')
+content = f.read()
+dial_pos = 50
+z_count = 0
+
+for row in content.splitlines():
+    direction = row[0]
+    number = int(row[1:])%100
+
+    dial_pos = (dial_pos-number)%100 if direction == "L" else (dial_pos+number)%100
+
+    if(dial_pos == 0):
+        z_count += 1
+
+print(z_count)
