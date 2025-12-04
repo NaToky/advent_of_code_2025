6ad1167c3b05bf7f01a33a4bdcadd96c3f45ebff
NaToKy <panser12@hotmail.com>
2025-12-04 14:41:50 -0500

    part 2

---
+++ day1_p2.py
@@ -0,0 +1,18 @@
+f = open('input_day_1.txt', 'r')
+content = f.read()
+
+dial_pos = 50
+z_count = 0
+
+for row in content.splitlines():
+    direction = row[0]
+    number = int(row[1:])
+
+    step = 1 if direction == "R" else -1
+
+    for _ in range(1, number + 1):
+        dial_pos += step
+        dial_pos %= 100
+        z_count += dial_pos == 0
+
+print(z_count)
